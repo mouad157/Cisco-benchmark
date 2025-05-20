5a6011954aab821885cc234af3b52b4c9ccb3584
mouad157 <94315381+mouad157@users.noreply.github.com>
2025-05-20 12:54:12 +0800

    Add files via upload

---
+++ TextChunker/Config.py
@@ -0,0 +1,13 @@
+## openai params
+# openai model name for metadata extraction and section name look up
+modelname="gpt-4o-mini"
+# openai API key
+openai_key = "your-openai-key"
+## I/O params
+# input file path .txt or .pdf
+inptxtfile="./path/to/input.txt"
+# output folder path
+outdir="./path/to/folder"
+## section param
+# typical average section length
+avgseclen = 2048
